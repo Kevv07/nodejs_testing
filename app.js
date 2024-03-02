adb0aaf75e87edc0eea4cecd3ca9c0243248832b
Kevin <plasenciakevin007@gmail.com>
2024-03-02 13:45:26 +0000

    nodejs and express testing

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.get('/currenttime', function(request, response) {
+    response.send('<h1>'+ new Date().toISOString() +'</h1>');
+}); //localhost:3000/currenttime
+
+app.get('/', function(request, response){
+    response.send('<form action="/store-user" method="POST"><label>Your Name>/label><input type="text" name="username"><button>Submit</button></form>');
+});  //localhost:3000/
+
+app.post('/store-user', function(request, response) {
+    const userName = request.body.username;
+    console.log(userName);
+    response.send('<h1>Username stored!</h1>');
+});
+
+app.listen(3000)
